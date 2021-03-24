94951d691387e79620ad1bbdd73b3c4537a203bc
Vavyo <vavyo2121@gmail.com>
2021-03-24 17:15:24 -0500

    pre test product

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -2,11 +2,87 @@
 
 namespace Challenges
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter 3 numbers: ");
+            string userInput = Console.ReadLine();
+            string[] splitInput = userInput.Split(' ');
+            int[] inputNums = new int[splitInput.Length];
+
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    inputNums[i] = Convert.ToInt32(splitInput[i]);
+                }
+                catch (FormatException)
+                {
+                    inputNums[i] = 1;
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine($"Overflow error: {e.Message}");
+                }
+            }
+            Console.WriteLine($"The product of these 3 numbers is: {Product(inputNums)}");
+
         }
+        public static int Product(int[] arr)
+        {
+            int result = 1;
+            if (arr.Length > 2)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    result *= arr[i];
+                }
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public static int Average()
+        {
+
+            return 0;
+        }
+        public static void Diamond()
+        {
+
+        }
+        public static int Most(int[] arr)
+        {
+
+            return 0;
+        }
+        public static int Max()
+        {
+
+            return 0;
+        }
+        public static void Save()
+        {
+
+        }
+        public static void Load()
+        {
+
+        }
+        public static void Remove()
+        {
+
+        }
+
+        public static string[] WordLengths()
+        {
+            string[] result = new string[0];
+            return result;
+        }
+
+
     }
 }
--- ChallengesTests/UnitTest1.cs
+++ ChallengesTests/UnitTest1.cs
@@ -1,14 +1,19 @@
 using System;
 using Xunit;
+using Challenges;
 
 namespace ChallengesTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Product_returns_the_product()
         {
+            // arrange
+            int[] arr = new int[] { 1, 2, 3 };
 
+            // act
+            int result = Program.Product(arr);
         }
     }
 }
